3ce7841ae60d47b6ad6fbe705fcc34d06a0f633f
Omar Wafaey <omarwafaey54@gmail.com>
2022-05-22 02:18:55 +0200

    Add effects file in overmind

---
+++ src/store/effects.ts
@@ -0,0 +1,8 @@
+import Request from "./config";
+import { Ticker, TickerInfo, TickerStatistics } from "../store/state";
+import { BaseURL, API_KEY } from "../utils/constants";
+
+export const storage = {
+
+};
+
