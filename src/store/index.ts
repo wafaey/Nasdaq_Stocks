f500833903b70d680fbd17b3b9d31145813b88bd
Omar Wafaey <omarwafaey54@gmail.com>
2022-05-22 02:19:27 +0200

    Add overmind configuration.

---
+++ src/store/index.ts
@@ -0,0 +1,20 @@
+import { createStateHook, createActionsHook } from 'overmind-react';
+import { state } from './state';
+import * as actions from './actions';
+import * as effects from './effects';
+import { IContext } from 'overmind';
+
+export const config = {
+  state,
+  actions,
+  effects,
+};
+
+export type Context = IContext<{
+  state: typeof config.state;
+  actions: typeof config.actions;
+  effects: typeof config.effects;
+}>;
+
+export const useAppState = createStateHook<Context>();
+export const useActions = createActionsHook<Context>();
